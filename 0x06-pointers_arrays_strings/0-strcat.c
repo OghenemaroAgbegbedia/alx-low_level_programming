741f5fd67a6ff8cb1d6cff2f4c157a576caa7c5c
OghenemaroAgbegebdi <marojude472@gmail.com>
2022-09-21 06:14:54 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,10 +3,10 @@
 /**
  * _strcat - Concatenates two strings
  * @dest: Destination string
+ * @src: Source string
  *
  * Return: A pointer to the resulting string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 int i;
